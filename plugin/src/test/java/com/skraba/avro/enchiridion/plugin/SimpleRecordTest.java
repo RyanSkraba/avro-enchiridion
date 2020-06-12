8bffc04b0384dc793fcfa7a7aef42255788b0156
Ryan Skraba <ryan@skraba.com>
2020-06-12 14:23:35 +0200

    feat: Use staging for 1.10.0 release

---
+++ plugin/src/test/java/com/skraba/avro/enchiridion/plugin/SimpleRecordTest.java
@@ -0,0 +1,29 @@
+package com.skraba.avro.enchiridion.plugin;
+
+import com.skraba.avro.enchiridion.simple.SimpleRecord;
+import org.apache.avro.generic.GenericRecord;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.instanceOf;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.nullValue;
+
+public class SimpleRecordTest {
+
+  @Test
+  public void testSimpleRecord() {
+    // This should be generated as a test resource.
+    SimpleRecord sr = new SimpleRecord();
+    assertThat(sr, instanceOf(GenericRecord.class));
+    assertThat(sr.id, is(0L));
+    assertThat(sr.name, nullValue());
+  }
+
+  @Test
+  public void testSimpleRecordConstructor() {
+    SimpleRecord sr = new SimpleRecord(123L, "abc");
+    assertThat(sr.id, is(123L));
+    assertThat(sr.name, is("abc"));
+  }
+}
