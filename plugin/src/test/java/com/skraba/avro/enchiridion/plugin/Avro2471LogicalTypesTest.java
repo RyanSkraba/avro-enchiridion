540adbe936fb5664ad9b9d79598fabad3bd7671d
Ryan Skraba <ryan@skraba.com>
2020-11-09 18:24:54 +0100

    feat: Test schemas with keywords.

--- plugin/src/test/java/com/skraba/avro/enchiridion/plugin/Avro2471LogicalTypesTest.java
+++ plugin/src/test/java/com/skraba/avro/enchiridion/plugin/Avro2471LogicalTypesTest.java
@@ -1,51 +1,23 @@
 package com.skraba.avro.enchiridion.plugin;
 
+import static com.skraba.avro.enchiridion.plugin.SimpleRecordTest.fromBytes;
+import static com.skraba.avro.enchiridion.plugin.SimpleRecordTest.toBytes;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
 
-import com.skraba.avro.enchiridion.idl.*;
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
+import com.skraba.avro.enchiridion.idl.TimestampMicrosOptional;
+import com.skraba.avro.enchiridion.idl.TimestampMicrosRequired;
+import com.skraba.avro.enchiridion.idl.TimestampMillisOptional;
+import com.skraba.avro.enchiridion.idl.TimestampMillisRequired;
 import java.io.IOException;
 import java.time.Instant;
-import org.apache.avro.Schema;
 import org.apache.avro.data.TimeConversions;
-import org.apache.avro.io.DatumReader;
-import org.apache.avro.io.DatumWriter;
-import org.apache.avro.io.Decoder;
-import org.apache.avro.io.DecoderFactory;
-import org.apache.avro.io.Encoder;
-import org.apache.avro.io.EncoderFactory;
-import org.apache.avro.specific.SpecificData;
-import org.apache.avro.specific.SpecificDatumReader;
-import org.apache.avro.specific.SpecificDatumWriter;
 import org.junit.jupiter.api.Test;
 
+/** Unit tests related to AVRO-2471 and logical types. */
 public class Avro2471LogicalTypesTest {
   public static final Instant TS = Instant.EPOCH;
-
-  public static <T> byte[] toBytes(SpecificData model, Schema schema, T datum) {
-    try (ByteArrayOutputStream baos = new ByteArrayOutputStream()) {
-      Encoder encoder = EncoderFactory.get().binaryEncoder(baos, null);
-      DatumWriter<T> w = new SpecificDatumWriter<T>(schema, model);
-      w.write(datum, encoder);
-      encoder.flush();
-      return baos.toByteArray();
-    } catch (IOException ioe) {
-      throw new RuntimeException((ioe));
-    }
-  }
-
-  public static <T> T fromBytes(SpecificData model, Schema schema, byte[] serialized) {
-    try (ByteArrayInputStream bais = new ByteArrayInputStream(serialized)) {
-      Decoder decoder = DecoderFactory.get().binaryDecoder(bais, null);
-      DatumReader<T> r = new SpecificDatumReader<T>(schema, schema, model);
-      return r.read(null, decoder);
-    } catch (IOException ioe) {
-      throw new RuntimeException((ioe));
-    }
-  }
 
   @Test
   public void testTimestampMillisRequired() throws IOException {
@@ -83,25 +55,4 @@ public class Avro2471LogicalTypesTest {
     // Round-trip should reconstitute an equal instance.
     assertThat(fromBytes(f.getSpecificData(), f.getSchema(), serialized), is(f));
   }
-
-  //  @Test
-  //  public void foobar2x() throws IOException {
-  //    final Foobar2 f = Foobar2.newBuilder().setId("aaa").setTs(Instant.now()).build();
-  //    f.getSpecificData().addLogicalTypeConversion(new
-  // TimeConversions.TimestampMicrosConversion());
-  //    byte[] serialized;
-  //    try (final ByteArrayOutputStream out = new ByteArrayOutputStream()) {
-  //      SpecificDatumWriter<SpecificRecord> writer =
-  //          new SpecificDatumWriter<>(((SpecificRecord) f).getSchema());
-  //      Encoder encoder = EncoderFactory.get().binaryEncoder(out, null);
-  //      writer.write(f, encoder);
-  //      serialized = out.toByteArray();
-  //    }
-  //
-  //    try (ByteArrayInputStream bais = new ByteArrayInputStream(serialized)) {
-  //      Decoder decoder = DecoderFactory.get().binaryDecoder(bais, null);
-  //      SpecificDatumReader<Foobar2> r = new SpecificDatumReader<>(Foobar2.class);
-  //      Foobar2 floopy = r.read(null, decoder);
-  //    }
-  //  }
 }
--- plugin/src/test/java/com/skraba/avro/enchiridion/plugin/SimpleRecordTest.java
+++ plugin/src/test/java/com/skraba/avro/enchiridion/plugin/SimpleRecordTest.java
@@ -1,15 +1,57 @@
 package com.skraba.avro.enchiridion.plugin;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.instanceOf;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.nullValue;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import com.skraba.avro.enchiridion.simple.SimpleRecord;
+import com.skraba.avro.enchiridion.simple.case$;
+import com.skraba.avro.enchiridion.simple.static$;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import org.apache.avro.Schema;
 import org.apache.avro.generic.GenericRecord;
+import org.apache.avro.io.DatumReader;
+import org.apache.avro.io.DatumWriter;
+import org.apache.avro.io.Decoder;
+import org.apache.avro.io.DecoderFactory;
+import org.apache.avro.io.Encoder;
+import org.apache.avro.io.EncoderFactory;
+import org.apache.avro.specific.SpecificData;
+import org.apache.avro.specific.SpecificDatumReader;
+import org.apache.avro.specific.SpecificDatumWriter;
 import org.junit.jupiter.api.Test;
 
 public class SimpleRecordTest {
+
+  public static final SimpleRecord SIMPLE = new SimpleRecord(123L, "abc");
+
+  public static <T> byte[] toBytes(SpecificData model, Schema schema, T datum) {
+    try (ByteArrayOutputStream baos = new ByteArrayOutputStream()) {
+      Encoder encoder = EncoderFactory.get().binaryEncoder(baos, null);
+      DatumWriter<T> w = new SpecificDatumWriter<T>(schema, model);
+      w.write(datum, encoder);
+      encoder.flush();
+      return baos.toByteArray();
+    } catch (IOException ioe) {
+      throw new RuntimeException((ioe));
+    }
+  }
+
+  public static <T> T fromBytes(SpecificData model, Schema schema, byte[] serialized) {
+    try (ByteArrayInputStream bais = new ByteArrayInputStream(serialized)) {
+      Decoder decoder = DecoderFactory.get().binaryDecoder(bais, null);
+      DatumReader<T> r = new SpecificDatumReader<T>(schema, schema, model);
+      return r.read(null, decoder);
+    } catch (IOException ioe) {
+      throw new RuntimeException((ioe));
+    }
+  }
 
   @Test
   public void testSimpleRecord() {
@@ -25,5 +67,56 @@ public class SimpleRecordTest {
     SimpleRecord sr = new SimpleRecord(123L, "abc");
     assertThat(sr.id, is(123L));
     assertThat(sr.name, is("abc"));
+  }
+
+  @Test
+  public void testSimpleRecordConstructorCloneViaBytes() {
+    SimpleRecord clone =
+        fromBytes(
+            SIMPLE.getSpecificData(),
+            SIMPLE.getSchema(),
+            toBytes(SIMPLE.getSpecificData(), SIMPLE.getSchema(), SIMPLE));
+
+    assertThat(clone, equalTo(SIMPLE));
+  }
+
+  @Test
+  public void testSimpleRecordConstructorCloneViaMessage() throws IOException {
+    ByteBuffer bb = SIMPLE.toByteBuffer();
+    bb.rewind();
+
+    SimpleRecord clone = SimpleRecord.fromByteBuffer(bb);
+
+    assertThat(clone, equalTo(SIMPLE));
+  }
+
+  @Test
+  public void testReservedKeywordNames() {
+    case$ inner = new case$();
+    assertThat(inner, instanceOf(GenericRecord.class));
+    assertThat(inner.default$, is(0L));
+
+    static$ wrapper = new static$();
+    assertThat(wrapper, instanceOf(GenericRecord.class));
+    assertThat(wrapper.switch$, nullValue());
+
+    wrapper.setSwitch$(inner);
+
+    // This is the bug behaviour.
+    assertThrows(
+        ClassCastException.class,
+        () -> {
+          assertThat(
+              fromBytes(
+                  wrapper.getSpecificData(),
+                  wrapper.getSchema(),
+                  toBytes(wrapper.getSpecificData(), wrapper.getSchema(), wrapper)),
+              equalTo(wrapper));
+
+          byte[] bytes = toBytes(wrapper.getSpecificData(), wrapper.getSchema(), wrapper);
+          assertThat(bytes.length, is(1));
+          static$ cloned = fromBytes(wrapper.getSpecificData(), wrapper.getSchema(), bytes);
+          assertThat(cloned, equalTo(wrapper));
+        });
   }
 }
