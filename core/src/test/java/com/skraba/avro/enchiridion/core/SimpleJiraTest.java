954760f86562c7a8e4c302e2c70214db315f45fe
Ryan Skraba <ryan@skraba.com>
2020-12-02 17:23:02 +0100

    feat: Add unit test for Map equality

--- core/src/test/java/com/skraba/avro/enchiridion/core/SimpleJiraTest.java
+++ core/src/test/java/com/skraba/avro/enchiridion/core/SimpleJiraTest.java
@@ -3,9 +3,16 @@ package com.skraba.avro.enchiridion.core;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.not;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import java.util.HashMap;
+import java.util.Map;
+import org.apache.avro.AvroRuntimeException;
 import org.apache.avro.Schema;
 import org.apache.avro.SchemaBuilder;
+import org.apache.avro.generic.GenericData;
+import org.apache.avro.generic.GenericRecord;
+import org.apache.avro.util.Utf8;
 import org.junit.jupiter.api.Test;
 
 public class SimpleJiraTest {
@@ -41,5 +48,35 @@ public class SimpleJiraTest {
       assertThat(
           aCopy.getField("b").schema().getField("c").schema().getFullName(), is("default.c"));
     }
+  }
+
+  /** @see <a href="https://issues.apache.org/jira/browse/AVRO-2943">AVRO-2943</a> */
+  @Test
+  public void testAvro2943MapEquality() {
+    Schema schema =
+        SchemaBuilder.record("A")
+            .fields()
+            .name("a1")
+            .type()
+            .map()
+            .values()
+            .stringType()
+            .noDefault()
+            .endRecord();
+
+    GenericRecord record1 = new GenericData.Record(schema);
+    record1.put(0, new HashMap<CharSequence, String>());
+    ((Map<CharSequence, String>) record1.get(0)).put("id", "one");
+    GenericRecord record2 = new GenericData.Record(schema);
+    record2.put(0, new HashMap<CharSequence, String>());
+    ((Map<CharSequence, String>) record2.get(0)).put(new Utf8("id"), "one");
+
+    AvroRuntimeException ex =
+        assertThrows(
+            AvroRuntimeException.class,
+            () -> assertThat(GenericData.get().compare(record1, record2, schema), is(0)));
+    assertThat(ex.getMessage(), is("Can't compare maps!"));
+    assertThat(record1, not(record2));
+    assertThat(record2, not(record1));
   }
 }
