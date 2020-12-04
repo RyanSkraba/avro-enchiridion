2d090234913957f9279cf8a5174e74b2c3ef2f24
Ryan Skraba <ryan@skraba.com>
2020-12-04 13:25:31 +0100

    feat(AVRO-2956): Reproduce serialization failure in IDL

---
+++ plugin/src/test/java/com/skraba/avro/enchiridion/plugin/Avro2956ReservedKeywordTest.java
@@ -0,0 +1,40 @@
+package com.skraba.avro.enchiridion.plugin;
+
+import static com.skraba.avro.enchiridion.plugin.SimpleRecordTest.fromBytes;
+import static com.skraba.avro.enchiridion.plugin.SimpleRecordTest.toBytes;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import com.skraba.avro.enchiridion.idl.Avro2956ReservedKeywordWrapper;
+import com.skraba.avro.enchiridion.idl.case$;
+import java.io.IOException;
+import org.junit.jupiter.api.Test;
+
+/** Unit tests related to AVRO-2956 */
+public class Avro2956ReservedKeywordTest {
+
+  @Test
+  public void testBasic() throws IOException {
+    Avro2956ReservedKeywordWrapper wrapper =
+        Avro2956ReservedKeywordWrapper.newBuilder()
+            .setInner(case$.newBuilder().setValue("AVRO-2956").build())
+            .build();
+    byte[] serialized = toBytes(wrapper.getSpecificData(), wrapper.getSchema(), wrapper);
+    assertThat(serialized.length, equalTo(10));
+
+    // Round-trip should reconstitute an equal instance, but this throws a ClassCastException
+    ClassCastException ex =
+        assertThrows(
+            ClassCastException.class,
+            () ->
+                assertThat(
+                    fromBytes(wrapper.getSpecificData(), wrapper.getSchema(), serialized),
+                    is(wrapper)));
+    assertThat(
+        ex.getMessage(),
+        is(
+            "org.apache.avro.generic.GenericData$Record cannot be cast to com.skraba.avro.enchiridion.idl.case$"));
+  }
+}
