5e85e7710561d5468e4b38e30deaab2b6123d18d
Ryan Skraba <ryan@skraba.com>
2021-03-24 18:05:14 +0100

    feat: Clean up reserved keywords test

--- plugin/src/test/java/com/skraba/avro/enchiridion/plugin/Avro2956ReservedKeywordTest.java
+++ plugin/src/test/java/com/skraba/avro/enchiridion/plugin/Avro2956ReservedKeywordTest.java
@@ -1,40 +1,76 @@
 package com.skraba.avro.enchiridion.plugin;
 
+import com.skraba.avro.enchiridion.idl.Avro2956ReservedKeywordWrapper;
+import com.skraba.avro.enchiridion.iddl.goto$.case$;
+import com.skraba.avro.enchiridion.idl.break$.static$;
+
+import org.apache.avro.generic.GenericData;
+import org.apache.avro.generic.GenericRecord;
+import org.apache.avro.specific.SpecificData;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
 import static com.skraba.avro.enchiridion.plugin.SimpleRecordTest.fromBytes;
 import static com.skraba.avro.enchiridion.plugin.SimpleRecordTest.toBytes;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.instanceOf;
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.nullValue;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-
-import com.skraba.avro.enchiridion.idl.Avro2956ReservedKeywordWrapper;
-import com.skraba.avro.enchiridion.idl.case$;
-import java.io.IOException;
-import org.junit.jupiter.api.Test;
 
 /** Unit tests related to AVRO-2956 */
 public class Avro2956ReservedKeywordTest {
 
   @Test
-  public void testBasic() throws IOException {
-    Avro2956ReservedKeywordWrapper wrapper =
-        Avro2956ReservedKeywordWrapper.newBuilder()
-            .setInner(case$.newBuilder().setValue("AVRO-2956").build())
-            .build();
-    byte[] serialized = toBytes(wrapper.getSpecificData(), wrapper.getSchema(), wrapper);
-    assertThat(serialized.length, equalTo(10));
+  public void testBasic() {
 
-    // Round-trip should reconstitute an equal instance, but this throws a ClassCastException
+    // Start with a simple serialized long.
+    byte[] serialized = {0};
+
+    // This is equivalent to this record.
+    Avro2956ReservedKeywordWrapper record =
+        Avro2956ReservedKeywordWrapper.newBuilder()
+            .setInner(case$.newBuilder().setDefault$(0).build())
+            .build();
+    assertThat(toBytes(record.getSpecificData(), record.getSchema(), record), is(serialized));
+
+    // Problem one: round-trip should reconstitute an equal instance, but this throws a
+    // ClassCastException.
     ClassCastException ex =
         assertThrows(
             ClassCastException.class,
-            () ->
-                assertThat(
-                    fromBytes(wrapper.getSpecificData(), wrapper.getSchema(), serialized),
-                    is(wrapper)));
+            () -> fromBytes(record.getSpecificData(), record.getSchema(), serialized));
     assertThat(
         ex.getMessage(),
         containsString("org.apache.avro.generic.GenericData$Record cannot be cast"));
+
+    // This should be structurally identical to the above record.
+    static$ record2 =
+        static$.newBuilder().setSwitch$(case$.newBuilder().setDefault$(0).build()).build();
+    assertThat(toBytes(record2.getSpecificData(), record.getSchema(), record), is(serialized));
+
+    // Round-trip should reconstitute an equal instance, but this throws a ClassCastException.
+    ex =
+        assertThrows(
+            ClassCastException.class,
+            () -> {
+              // Note that we've specified the expected return type.
+              static$ unused =
+                  fromBytes(record2.getSpecificData(), record2.getSchema(), serialized);
+            });
+    assertThat(
+        ex.getMessage(),
+        containsString("org.apache.avro.generic.GenericData$Record cannot be cast"));
+
+    // Problem two: we can deserialize to GenericRecord and even compare it to the original.
+    GenericRecord record3 = fromBytes(record2.getSpecificData(), record2.getSchema(), serialized);
+    assertThat(record3, instanceOf(GenericData.Record.class));
+    assertThat(SpecificData.get().compare(record2, record3, static$.getClassSchema()), is(0));
+
+    // Problem three: but they aren't equal.
+    assertThat(record2.equals(record3), is(false));
   }
 }
