4dfbf00057b91d58e84dc02d276642e541063e3c
Ryan Skraba <ryan@skraba.com>
2021-04-01 15:11:52 +0200

    feat: Add echo to logical types protocol

--- plugin/src/test/java/com/skraba/avro/enchiridion/plugin/Avro2471LogicalTypesTest.java
+++ plugin/src/test/java/com/skraba/avro/enchiridion/plugin/Avro2471LogicalTypesTest.java
@@ -5,14 +5,23 @@ import static com.skraba.avro.enchiridion.plugin.SimpleRecordTest.toBytes;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import com.skraba.avro.enchiridion.idl.Avro2471LogicalTypes;
+import com.skraba.avro.enchiridion.idl.TimestampAll;
 import com.skraba.avro.enchiridion.idl.TimestampMicrosOptional;
 import com.skraba.avro.enchiridion.idl.TimestampMicrosRequired;
 import com.skraba.avro.enchiridion.idl.TimestampMillisOptional;
 import com.skraba.avro.enchiridion.idl.TimestampMillisRequired;
 import java.io.IOException;
 import java.time.Instant;
+import org.apache.avro.AvroRuntimeException;
 import org.apache.avro.data.TimeConversions;
+import org.apache.avro.ipc.LocalTransceiver;
+import org.apache.avro.ipc.Responder;
+import org.apache.avro.ipc.specific.SpecificRequestor;
+import org.apache.avro.ipc.specific.SpecificResponder;
 import org.junit.jupiter.api.Test;
 
 /** Unit tests related to AVRO-2471 and logical types. */
@@ -54,5 +63,27 @@ public class Avro2471LogicalTypesTest {
     assertThat(serialized.length, equalTo(5));
     // Round-trip should reconstitute an equal instance.
     assertThat(fromBytes(f.getSpecificData(), f.getSchema(), serialized), is(f));
+  }
+
+  @Test
+  public void testRequestResponse() throws IOException {
+    // Create the server side that knows how to respond to messages
+    Responder responder =
+        new SpecificResponder(Avro2471LogicalTypes.class, (Avro2471LogicalTypes) in -> in);
+
+    // Create the client side to make requests via the local transceiver.
+    LocalTransceiver transceiver = new LocalTransceiver(responder);
+    Avro2471LogicalTypes requestor =
+        SpecificRequestor.getClient(Avro2471LogicalTypes.class, transceiver);
+
+    // These IPC requests succeed.
+    assertThat(requestor.echo(TimestampAll.newBuilder().build()), notNullValue());
+    assertThat(
+        requestor.echo(TimestampAll.newBuilder().setTimeUs(Instant.now()).build()), notNullValue());
+
+    // But this one fails.
+    assertThrows(
+        AvroRuntimeException.class,
+        () -> requestor.echo(TimestampAll.newBuilder().setTimeUsOpt(Instant.now()).build()));
   }
 }
